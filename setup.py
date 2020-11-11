aabf62f9b963583714f87d384307e2966b2662cc
Julien <julien.bellanger@cornershopapp.com>
2020-11-11 12:36:45 -0300

    feature (shopper-payments-integration): base project
    corrections lib

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="shopper_payments_integration",
-    version="0.1dev2",
+    version="0.3dev",
     packages=setuptools.find_packages(),
     license="",
     long_description=open("README.md").read(),
