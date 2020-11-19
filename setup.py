22bbec87bb2dcd50b504581f1262851a0206f723
Julien <julien.bellanger@cornershopapp.com>
2020-11-19 15:32:23 -0300

    feature (shopper-payments-integration) correction import lib
    add pip package
    -arrow
    -stdnum
    -phonenumbers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="shopper_payments_integration",
-    version="0.5dev",
+    version="0.6dev",
     packages=setuptools.find_packages(),
     license="",
     long_description=open("README.md").read(),
