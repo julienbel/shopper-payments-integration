de0dddbe54fe83b4a449a7fb7aa9f4f7d8d4dca7
Julien <julien.bellanger@cornershopapp.com>
2020-11-20 12:11:17 -0300

    bug (shopper-payments-integration): correction error during refactor
    correction error when refactoring for splitting the micro-service in 3 repositories

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="shopper_payments_integration",
-    version="0.8dev1",
+    version="0.8dev2",
     packages=setuptools.find_packages(),
     license="",
     long_description=open("README.md").read(),
