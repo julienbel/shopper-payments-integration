1b5b7c2afe6cf2cbfbc8cbfbe2b935b1fd638039
Julien <julien.bellanger@cornershopapp.com>
2020-12-21 13:59:44 -0300

    corrections review

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="shopper_payments_integration",
-    version="0.9dev7",
+    version="0.9dev8",
     packages=setuptools.find_packages(),
     license="",
     long_description=open("README.md").read(),
