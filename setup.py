7be377960a43ea607916818bf1d3c5b817845e23
Jefferson <dvjefferson@gmail.com>
2020-12-23 18:05:28 -0300

    chore(setup): upload package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="shopper_payments_integration",
-    version="1.1dev",
+    version="1.2dev",
     packages=setuptools.find_packages(),
     license="",
     long_description=open("README.md").read(),
