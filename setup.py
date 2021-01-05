274be00987151a1eef4349f0b34730e15d54361a
Julien <julien.bellanger@cornershopapp.com>
2021-01-05 18:56:23 -0300

    chore (shopper-payments-integration) correction pre-commit
    corrections logger
    corrections pre-commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="shopper_payments_integration",
-    version="1.8dev1",
+    version="1.8dev2",
     packages=setuptools.find_packages(),
     license="",
     long_description=open("README.md").read(),
